8c5d70e6e88f57dfd3fb2762d7d3caa929e99e9f
lingyaintocareers <38334126+lingyaintocareers@users.noreply.github.com>
2018-12-06 16:02:29 -0800

    issue #12

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { goDucks } = require('./go-ducks');
+goDucks.appendMessages([() => 'EVERYBODY']);
 goDucks.appendMessages([() => 'A']);
 goDucks.appendMessages([() => 'IT\'S']);
 console.log(goDucks.quack());
